afe289ad874c3c8b51d8ed98d99b9d7db4d355e6
AbhinavRaha <89091790+AbhinavRaha@users.noreply.github.com>
2022-01-10 15:04:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+ mouseEvent="empty";
+var lastX,lastY;
+canvas = document.getElementById('myCanvas'); 
+ ctx = canvas.getContext("2d");
+ color = "red";
+ width_of_line = 10;
+ canvas.addEventListener("mousedown", my_mousedown);
+ function my_mousedown(e)
+ {
+   mouseEvent="mousDown";
+ }
+ canvas.addEventListener("mousemove",my_mousemove);
+ function my_mousemove(e)
+ {
+current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+if (mouseEvent == "mouseDown")
+ { ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = width_of_line;
+ctx.moveTo(lastX, lastY);
+ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y); ctx.stroke();
+}
+lastX = current_position_of_mouse_x; lastY = current_position_of_mouse_y;
+ }
+ 
